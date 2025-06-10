8a59442a7f88384ab1dcf36fe953e216a9b31931
kwon koohyun <56860056+implement-k@users.noreply.github.com>
2025-06-10 11:39:47 +0900

    Create 4_14675_단절점과단절선.cpp

---
+++ 권구현/9주차/4_14675_단절점과단절선.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int N; cin >> N;
+    for(int i = 0; i < N-1; i++){
+        int u,v; cin >> u >> v;
+    }
+    int Q; cin >> Q;
+    while(Q--){
+        int t,k; cin >> t >> k;
+        cout << (t==1?"yes":"no") << "\n";
+    }
+}
